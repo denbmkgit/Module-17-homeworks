d63d8ef612af95f2511b684266d8cfe50552551c
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-07 20:49:00 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,6 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
@@ -18,3 +17,45 @@ class User(Base):
 from sqlalchemy.schema import CreateTable
 
 print(CreateTable(User.__table__))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# from app.backend.db import Base
+# from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+# from sqlalchemy.orm import relationship
+# # from app.models import *
+# # from app.models.task import *
+#
+#
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String)
+#     firstname = Column(String)
+#     lastname = Column(String)
+#     age = Column(Integer)
+#     slug = Column(String, unique=True, index=True)
+#
+#     tasks = relationship('Task', back_populates='user')
+#
+# from sqlalchemy.schema import CreateTable
+#
+# print(CreateTable(User.__table__))
