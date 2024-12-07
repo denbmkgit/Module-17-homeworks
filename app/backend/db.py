1ea4d2597624289a6231195e3c0919409b9eefdf
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-12-07 20:42:14 +0300

    07 12 2024 20_02- not ok

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine("sqlite:///taskmanager.db'", echo=True)
-
+Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 
-class Base(DeclarativeBase):
-    pass
+# class Base(DeclarativeBase):
+#     pass
